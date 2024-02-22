1ec03e690cb7067b314accdaf1c8cea1753e76b0
Claudio9616 <clares9611@gmail.com>
2024-02-22 17:03:41 +0100

    cors policy

--- api/index.php
+++ api/index.php
@@ -1,13 +1,22 @@
 <?php
-// individuo il path del JSON
-// $json_path = __DIR__ . '/../data/tasks.json';
-// prendo i suoi contenuti
-// $jason_data = file_get_contents($json_path)
-// $tasks = $jason_data
-$tasks = [
-    'id' => '1',
-    'text' => 'HTML',
-    'done' => 'false'
-];
+header("Access-Control-Allow-Origin: http://127.0.0.1:5000");
+header("Access-Control-Allow-Headers: X-Requested-With");
+// Imposta l'header per rispondere con JSON
 header('Content-Type: application/json');
+
+// Il path al file JSON
+$json_path = __DIR__ . '/../data/tasks.json';
+
+// Controlla se il file esiste e se è leggibile
+if (file_exists($json_path) && is_readable($json_path)) {
+    // Prende i contenuti del file JSON
+    $json_data = file_get_contents($json_path);
+    // Decodifica i dati JSON in un array associativo
+    $tasks = json_decode($json_data, true);
+} else {
+    // Gestisce l'errore se il file non esiste o non è leggibile
+    $tasks = ['error' => 'Unable to load tasks.'];
+}
+
+// Invia la risposta JSON
 echo json_encode($tasks);
--- my_app/script.js
+++ my_app/script.js
@@ -1,10 +1,14 @@
 const { createApp } = Vue
+const enpoint = 'http://127.0.0.1:80/boolean/php-todo-list-json/api/'
 const app = createApp({
     name: 'TO DO LIST',
-    data() {
-        return {
-
-        }
+    data: () => ({
+        tasks: []
+    }),
+    created() {
+        axios.get(enpoint).then(res => {
+            this.tasks = res.data
+        })
     }
 })
 app.mount('#root')
